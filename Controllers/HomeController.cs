3105c9ed0acb9aef8645dfa053e3da62483c94a1
Adriano Maurmann <adriano.maurmann@ecs.com.br>
2021-05-19 21:19:24 -0300

    exercicio concluido

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using DotNet5IdentityJWT.Models;
+using DotNet5IdentityJWT.Repositories;
+using DotNet5IdentityJWT.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DotNet5IdentityJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous()
+        {
+            return "Anônimo";
+        }
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated()
+        {
+            return String.Format("Autenticado - {0}", User.Identity.Name);
+        }
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee()
+        {
+            return "Funcionário";
+        }
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager()
+        {
+            return "Gerente";
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public ActionResult<dynamic> Authenticate([FromBody] User model)
+        {
+            // Recupera o usuário
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            // Verifica se o usuário existe
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = TokenService.GenerateToken(user);
+
+            // Oculta a senha
+            user.Password = "";
+
+            // Retorna os dados
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
